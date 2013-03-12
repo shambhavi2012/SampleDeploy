56d5cb9c7c2e0667dcf71d866749ef8fee4169f0
shambhavi <shambhavi2012@yahoo.co.in>
2013-03-12 10:17:59 +0530

    Sample Deploy on heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleDeploy::Application.config.secret_token = '7b6ebfd09210ac2be74ee91d6a36db7682d77b5cbc94b22798a7ba9787184e4941479185f69df739ff2126c138211d22df7620cfdd612df43b263ed4fee18480'
